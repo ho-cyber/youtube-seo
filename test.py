b5d9387bd1f944d7ba6cb2cc5bad88f54933b0e6
Dhruv Pai Dukle <dhruv@Dhruvs-MacBook-Pro.local>
2023-06-08 21:05:43 +0530

    added student mode

--- test.py
+++ test.py
@@ -1,3 +1,37 @@
-import os
 import streamlit as st
-os.path.dirname(st.file)
+
+def add_google_analytics_tracking_code(tracking_id):
+    """Add Google Analytics tracking code to the Streamlit app."""
+    script = """
+    <script async src="https://www.googletagmanager.com/gtag/js?id={tracking_id}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){{dataLayer.push(arguments);}}
+      gtag('js', new Date());
+      gtag('config', '{tracking_id}');
+    </script>
+    """.format(tracking_id=tracking_id)
+    st.markdown(script, unsafe_allow_html=True)
+
+def send_analytics_event(category, action):
+    """Send an analytics event to Google Analytics."""
+    script = """
+    <script>
+      if (typeof gtag !== 'undefined') {{
+        gtag('event', '{action}', {{
+          'event_category': '{category}',
+          'event_label': 'Button Click'
+        }});
+      }}
+    </script>
+    """.format(category=category, action=action)
+    st.markdown(script, unsafe_allow_html=True)
+
+# Usage example
+tracking_id = "G-M26G6BJJT0"
+add_google_analytics_tracking_code(tracking_id)
+
+# Add a button that sends an analytics event when clicked
+if st.button("Hello"):
+    send_analytics_event("Button", "Hello")
+    st.write("Button clicked!")
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -9,24 +9,56 @@ def download_youtube_video(url):
     try:
         youtube = pytube.YouTube(url)
         video = youtube.streams.get_highest_resolution()
+       
         video.download()
+        
         st.success('Video downloaded successfully!')
         # Rename the downloaded file to "video.mp4"
         default_filename = video.default_filename
         new_filename = "video.mp4"
         os.rename(default_filename, new_filename)
+        
+
+            
     except Exception as e:
         st.error(f'Error downloading video: {str(e)}')
 
+def download_youtube_video2(url):
+    try:
+        youtube = pytube.YouTube(url)
+        video = youtube.streams.get_highest_resolution()
+       
+        video.download()
+        
+        st.success('Video downloaded successfully!')
+        # Rename the downloaded file to "video.mp4"
+        default_filename = video.default_filename
+        new_filename = "video2.mp4"
+        os.rename(default_filename, new_filename)
+        
+
+            
+    except Exception as e:
+        st.error(f'Error downloading video: {str(e)}')
+
+mode = st.radio("Please select mode", ("Student", "Normal"))
+
 def main():
     st.title("GPTTUBE: Convert youtube videos into seo blog post in seconds")
+   
     url = st.text_input("Enter the YouTube video URL:")
-    if st.button("Download"):
-        if url:
-            download_youtube_video(url)
-        else:
-            st.warning("Please enter the YouTube video URL.")
-    
+    if mode == "Normal":
+        if st.button("Download"):
+            if url:
+                download_youtube_video(url)
+            else:
+                st.warning("Please enter the YouTube video URL.")
+    if mode == "Student":
+        if st.button("Download"):
+            if url:
+                download_youtube_video2(url)
+            else:
+                st.warning("Please enter the video url")
     ga_code = """
     <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-M26G6BJJT0"></script>
@@ -55,8 +87,11 @@ transcriber = aai.Transcriber()
 
 # If you have a local audio file, you can transcribe it using the code below.
 # Make sure to replace the filename with the path to your local audio file.
-transcript = transcriber.transcribe("./video.mp4")
+if mode == "Normal":
+    transcript = transcriber.transcribe("./video.mp4")
 
+if mode == "Student":
+    transcript = transcriber.transcribe("./video2.mp4")
 # Alternatively, if you have a URL to an audio file, you can transcribe it with the following code.
 # Uncomment the line below and replace the URL with the link to your audio file.
 # transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/espn-bears.m4a")
@@ -66,29 +101,39 @@ auth_token = st.secrets["auth_token"]
 openai.api_key = auth_token
 text = transcript.text
 
-final_ans = openai.Completion.create(
-    prompt = "Convert this into an SEO blog post also make it fun to read and intuitive while being seo friendly give it a mind-boggling title"+ text,
+if mode == "Normal":
+    final_ans = openai.Completion.create(
+    prompt = "Convert this into an SEO blog post also make it fun to read and intuitive while being seo friendly give it a suitable title"+ text,
     engine = "text-davinci-003",
     max_tokens = 500
 )
 
+if mode == "Student":
+    final_ans = openai.Completion.create(
+    prompt = "Give questions based on the transcript of this video they should be IMPORTANT QUESTIONS ONLY AND NOT SIDETRACKED QUESTIONS also generate a study plan for this with insights"+ text,
+    engine = "text-davinci-003",
+    max_tokens = 500
+)
+
+
 main_ans  = final_ans["choices"][0]["text"]
 st.write(main_ans)
-image_prompt  = openai.Completion.create(
+if mode == "Normal":
+    image_prompt  = openai.Completion.create(
     prompt = "Generate an image prompt for the following SEO blog post"+ main_ans,
     engine = "text-davinci-003",
     max_tokens = 500
 
 )
-proompt = image_prompt["choices"][0]["text"]
+    proompt = image_prompt["choices"][0]["text"]
 
-image = openai.Image.create(
+    image = openai.Image.create(
     prompt = proompt,
     size = "256x256"
 )
 
-image_url = image["data"][0]["url"]
+    image_url = image["data"][0]["url"]
 
-st.image(image_url)
-st.write(proompt)
+    st.image(image_url)
+    st.write(proompt)
 
